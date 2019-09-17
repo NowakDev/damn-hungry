577e90c3673d22340b290a70f03ac7fa886f6cc1
Kamil Nowak <it.kamilnowak@gmail.com>
2019-09-17 21:49:24 +0200

    home route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import ListOfRecipes from './containers/listOfRecipes/ListOfRecipes'
 import CreateRecipeForm from './containers/createRecipeForm/CreateRecipeForm'
@@ -10,8 +10,11 @@ const App = props => {
   return (
     <BrowserRouter>
       <NavBar />
-      <Route path={'/damn-hungry-recipes'} component={ListOfRecipes} />
-      <Route path={'/create-recipe-form'} component={CreateRecipeForm} />
+      <Switch>
+        <Route path={'/'} component={ListOfRecipes} exact />
+        <Route path={'/damn-hungry-recipes'} component={ListOfRecipes} />
+        <Route path={'/create-recipe-form'} component={CreateRecipeForm} />
+      </Switch>
     </BrowserRouter>
   )
 }
